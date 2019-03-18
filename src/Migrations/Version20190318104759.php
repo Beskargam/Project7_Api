eb44028e32fe829f16f7206049e4a8dd505c6c25
Beskargam <horg_hvergelmir@hotmail.com>
2019-03-18 11:50:03 +0100

    init jawsdb

--- src/Migrations/Version20190318104759.php
+++ src/Migrations/Version20190318104759.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190318102311 extends AbstractMigration
+final class Version20190318104759 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,18 +22,14 @@ final class Version20190318102311 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, manufacturer_id INT DEFAULT NULL, name VARCHAR(190) NOT NULL, description LONGTEXT NOT NULL, price INT NOT NULL, INDEX IDX_23A0E66A23B42D (manufacturer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE manufacturer (id INT AUTO_INCREMENT NOT NULL, manufacturer_name VARCHAR(190) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE token (id INT AUTO_INCREMENT NOT NULL, access_token VARCHAR(190) NOT NULL, date_token DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, access_token_id INT NOT NULL, vendor_id INT NOT NULL, username VARCHAR(190) NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, refresh_token VARCHAR(190) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_8D93D649C74F2195 (refresh_token), UNIQUE INDEX UNIQ_8D93D6492CCB2688 (access_token_id), INDEX IDX_8D93D649F603EE73 (vendor_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE vendor (id INT AUTO_INCREMENT NOT NULL, vendor_name VARCHAR(190) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE article ADD manufacturer_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E66A23B42D FOREIGN KEY (manufacturer_id) REFERENCES manufacturer (id)');
-        $this->addSql('CREATE INDEX IDX_23A0E66A23B42D ON article (manufacturer_id)');
-        $this->addSql('DROP INDEX UNIQ_8D93D649B6A2DD68 ON user');
-        $this->addSql('ALTER TABLE user ADD access_token_id INT NOT NULL, ADD vendor_id INT NOT NULL, DROP access_token');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6492CCB2688 FOREIGN KEY (access_token_id) REFERENCES token (id)');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649F603EE73 FOREIGN KEY (vendor_id) REFERENCES vendor (id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D6492CCB2688 ON user (access_token_id)');
-        $this->addSql('CREATE INDEX IDX_8D93D649F603EE73 ON user (vendor_id)');
     }
 
     public function down(Schema $schema) : void
@@ -44,14 +40,10 @@ final class Version20190318102311 extends AbstractMigration
         $this->addSql('ALTER TABLE article DROP FOREIGN KEY FK_23A0E66A23B42D');
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D6492CCB2688');
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649F603EE73');
+        $this->addSql('DROP TABLE article');
         $this->addSql('DROP TABLE manufacturer');
         $this->addSql('DROP TABLE token');
+        $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE vendor');
-        $this->addSql('DROP INDEX IDX_23A0E66A23B42D ON article');
-        $this->addSql('ALTER TABLE article DROP manufacturer_id');
-        $this->addSql('DROP INDEX UNIQ_8D93D6492CCB2688 ON user');
-        $this->addSql('DROP INDEX IDX_8D93D649F603EE73 ON user');
-        $this->addSql('ALTER TABLE user ADD access_token VARCHAR(190) NOT NULL COLLATE utf8mb4_unicode_ci, DROP access_token_id, DROP vendor_id');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649B6A2DD68 ON user (access_token)');
     }
 }
