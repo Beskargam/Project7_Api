30f9a28392da3135199e13d1b0c7be0b9fa616e1
Beskargam <horg_hvergelmir@hotmail.com>
2019-03-18 11:59:06 +0100

    init jawsdb

--- src/Migrations/Version20190318105834.php
+++ src/Migrations/Version20190318105834.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190318104759 extends AbstractMigration
+final class Version20190318105834 extends AbstractMigration
 {
     public function getDescription() : string
     {
