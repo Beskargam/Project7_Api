7a81d9d3c5625bece39ddd51693fa8b945181eae
Beskargam <horg_hvergelmir@hotmail.com>
2019-03-18 17:16:09 +0100

    fix route bug for the redirect uri mismatch

--- src/Security/GoogleUserProvider.php
+++ src/Security/GoogleUserProvider.php
@@ -48,7 +48,7 @@ class GoogleUserProvider implements UserProviderInterface
             $code,
             '755533673803-ilamn05m8fi64jk1hagaiidk0qjqk4u6.apps.googleusercontent.com',
             'dNcu3Ya1d5TuMSeMtTZIUzKh',
-            urlencode("https://app-bilemo.herokuapp.com/api/connexion/verification"),
+            urlencode("http://app-bilemo.herokuapp.com/api/connexion/verification"),
             'authorization_code'
         );
 
